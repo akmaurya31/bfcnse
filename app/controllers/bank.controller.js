8b77df601b1e22e03fc872082104f5387bacda76
rdminfosolution <rdminfosolution@gmail.com>
2020-09-25 12:26:59 +0530

    in com

---
+++ app/controllers/bank.controller.js
@@ -0,0 +1,20 @@
+const Master = require("../models/bank.model.js");
+
+// Create and Save a new Customer
+exports.bank = (req, res) => {
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  } 
+};
+
+// Retrieve all Customers from the database.
+exports.findAll = (req, res) => {
+  data['asdfasdf','sdfasdf','sdfasdf'];
+  res.send(data);
+};
+
+ 
+  
--- app/routes/customer.routes.js
+++ app/routes/customer.routes.js
@@ -1,11 +1,21 @@
 module.exports = app => {
   const customers = require("../controllers/customer.controller.js");
+  const bank = require("../controllers/bank.controller.js");
 
+  // Retrieve all Customers
+  app.get("/Bank", bank.findAll);
+
+  
   // Create a new Customer
   app.post("/customers", customers.create);
 
   // Retrieve all Customers
   app.get("/customers", customers.findAll);
+
+  // Retrieve all Customers
+  app.get("/customers", bank.findAll);
+
+
 
   // Retrieve a single Customer with customerId
   app.get("/customers/:customerId", customers.findOne);
