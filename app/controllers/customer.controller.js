e8ec6ebc5777af610ad879a3cf73fbe64ddb3c38
Dharm12 <53339580+Dharm12@users.noreply.github.com>
2020-09-29 17:21:42 +0530

    dharm

--- app/controllers/customer.controller.js
+++ app/controllers/customer.controller.js
@@ -57,7 +57,7 @@ exports.findOne = (req, res) => {
 };
 
 exports.findOne1users = (req, res) => {
-  Customer.findById(req.params.emailId, (err, data) => {
+  Customer.findByIdusers(req.params.emailId, (err, data) => {
     if (err) {
       if (err.kind === "not_found") {
         res.status(404).send({
